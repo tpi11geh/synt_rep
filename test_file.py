9a034e0c1a43d721fe8c443eb35d2ed23961117e
tpi11geh <tpi11geh@student.lu.se>
2017-05-27 18:05:03 +0200

    From test to start of letter creator
    From test to start of letter creator

--- test_file.py
+++ test_file.py
@@ -5,4 +5,14 @@ Created on Sat May 13 11:24:49 2017
 
 @author: Gustaf Ehn & Hugo Werner
 """
-hej hej
+import numpy as np
+
+M=26
+L=1000
+K=10*L
+t_pos=np.linspace(0,1,L)
+t_neg=np.linspace(-10,0,10*L)
+t=np.union1d(t_neg,t_pos)
+B=np.zeros(L+1)
+print(np.size(B))
+
