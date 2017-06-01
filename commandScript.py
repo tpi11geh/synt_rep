3d233a8653d9b7583cedf699d3bc38d673173637
tpi11geh <ehnzo@hotmail.com>
2017-06-01 16:15:46 +0200

    classify

---
+++ commandScript.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  1 11:18:45 2017
+
+@author: gustafehn
+"""
+import samplesGenerator as sg
+import numpy as np
+import sphinx
+
+nbr=10
+#singlesample=sg.Sample()
+#sample=singlesample.getMbm(4)
+#print(sample)
+ss=sg.SampleSet()
+Xsam=ss.genSamplesWithOutNorm(nbr)
+#
+sv=sg.SimulateVar()
+Xsam_normed=sv.normSet(nbr,Xsam)
+#
+#
+#Xsam_Uvar=ss.genSamples(nbr,sim_var)
+#s.samplePlot(nbr,Xsam_Uvar)
+#sim_varU=sv.getVar(nbr,Xsam_Uvar)
+#ss.samplePlot(1,sim_varU)
+ss.samplePlot(nbr,Xsam_normed)
+filename='DATA_Normed'
+ss.save(Xsam_normed,filename)
+
+#e=sg.Encrypt()
+#word=np.zeros([3,500])
+#count=0
+#w=np.array([8,5,10])
+#for i in range(3):
+#    word[i,:]=e.getEncWithOutNorm(w[i])
+#
+#e.plotEncryptedWord(word)
--- samplesGenerator.py
+++ samplesGenerator.py
@@ -11,98 +11,154 @@ import numpy as np
 import scipy.special as sps
 import matplotlib.pyplot as plt
 import json
-import time
-#
-start = time.time()
-M=26
-L=500
-K=10*L
-t_pos=np.linspace(0,1,L)
-t_neg=np.linspace(-10,0,10*L)
-t=np.union1d(t_neg,t_pos)
 
-def Hurst(x,letter):
-    case=letter%4
-    if case==0:
-        return 0.1*np.sin(2*np.pi*x*letter/4)+.5
-    elif case==1:
-        return 0.6-(.2/(1+np.exp(-600*(x-((letter+1)/28)))))
-    elif case==2:
-         return 0.4+(.2/(1+np.exp(-600*(x-(letter/28)))))
-    else:
-        return -0.1*np.sin(2*np.pi*x*((letter+3)/6))+.5
+class Encrypter(object):
+    
+    def __init__(self):
+        self.M=26
+        self.L=500
+        self.K=10*self.L
+        self.t_pos=np.linspace(0,1,self.L)
+        self.t_neg=np.linspace(-10,0,10*self.L)
+        self.t=np.union1d(self.t_neg,self.t_pos)
+
+    def Hurst(self,letter):
+        tn_pos=self.t_pos
+        case=letter%4
+        if case==0:
+            return 0.1*np.sin(2*np.pi*tn_pos*letter/4)+.5
+        elif case==1:
+            return 0.6-(.2/(1+np.exp(-600*(tn_pos-((letter+1)/28)))))
+        elif case==2:
+            return 0.4+(.2/(1+np.exp(-600*(tn_pos-(letter/28)))))
+        else:
+            return -0.1*np.sin(2*np.pi*tn_pos*((letter+3)/6))+.5
     
 
-def getMbm(letter):
-    B=np.zeros(L)
-    randVar=np.random.rand(K+L)-0.5
-    H_tn=Hurst(t_pos,letter)-0.5
-    for i in range(len(B)-1):
-        sum1=0
-        sum2=0
-        tn=t[K+i]
-        sum1=np.sum((np.power(tn-t_neg[:-1],H_tn[i])-np.power(-t_neg[:-1],H_tn[i]))*randVar[:K-1])
-        sum2=np.sum(np.power(tn-t_pos[:i+1],H_tn[i])*randVar[K:K+i+1])
-        B[i+1]=1/(sps.gamma(H_tn[i]+.5))*(1/np.sqrt(L))*(sum1+sum2)
-    return B
- 
-def getMbmVar(letter,var):
-    B=np.zeros(L)
-    randVar=np.random.rand(K+L)-0.5
-    H_tn=Hurst(t_pos,letter)-0.5
-    for i in range(len(B)-1):
-        sum1=0
-        sum2=0
-        tn=t[K+i]
-        sum1=np.sum((np.power(tn-t_neg[:-1],H_tn[i])-np.power(-t_neg[:-1],H_tn[i]))*randVar[:K-1])
-        sum2=np.sum(np.power(tn-t_pos[:i+1],H_tn[i])*randVar[K:K+i+1])
-        B[i+1]=(1/(sps.gamma(H_tn[i]+.5))*(1/np.sqrt(L))*(sum1+sum2)/np.sqrt(var[letter-1,i+1]))
-    return B
-
-def genSamples(n):
-    samples=np.zeros([n*M,L])
-    for i in range(M):
-        for j in range(n):
-            samples[i*n+j,:]=getMbm(i+1)
-        print(i)
-    return samples
-
-def genSamplesVar(n,var):
-    samples=np.zeros([n*M,L])
-    for i in range(M):
-        for j in range(n):
-            samples[i*n+j,:]=getMbmVar(i+1,var)
-        print(i)
-    return samples
-
-def getVar(nbrOfSam,samples):
-    simVar=np.zeros([M,L])
-    for i in range(M):
-        for j in range(0,L):
-            simVar[i,j]=np.var(samples[i*nbrOfSam:(i*nbrOfSam+nbrOfSam),j])
-    return simVar
-
+    def encryptWithOutNorm(self,letter):
+        L=self.L
+        K=self.K
+        t=self.t
+        t_pos=self.t_pos
+        t_neg=self.t_neg
         
-def save(saveData,filename):
-    ofile = open(filename, "w")
-    json.dump(saveData.tolist(), ofile, sort_keys = True, indent = 4)
-    ofile.close()
-
-def samplePlot(nbrOfsam,samples):
-    for i in range(M):
-        print('-------Sample path of one letter in a sample batch--------',i+1)
-        plt.plot(samples[i*nbrOfsam,:])
+        B=np.zeros(self.L)
+        randVar=np.random.rand(K+L)-0.5
+        H_tn=self.Hurst(letter)-0.5
+        for i in range(len(B)-1):
+            sum1=0
+            sum2=0
+            tn=t[K+i]
+            sum1=np.sum((np.power(tn-t_neg[:-1],H_tn[i])-np.power(-t_neg[:-1],H_tn[i]))*randVar[:K-1])
+            sum2=np.sum(np.power(tn-t_pos[:i+1],H_tn[i])*randVar[K:K+i+1])
+            B[i+1]=1/(sps.gamma(H_tn[i]+.5))*(1/np.sqrt(L))*(sum1+sum2)
+        return B
+ 
+    def encrypt(self,letter,var):
+        L=self.L
+        K=self.K
+        t=self.t
+        t_pos=self.t_pos
+        t_neg=self.t_neg
+        
+        B=np.zeros(L)
+        randVar=np.random.rand(K+L)-0.5
+        H_tn=self.Hurst(letter)-0.5
+        for i in range(len(B)-1):
+            sum1=0
+            sum2=0
+            tn=t[K+i]
+            sum1=np.sum((np.power(tn-t_neg[:-1],H_tn[i])-np.power(-t_neg[:-1],H_tn[i]))*randVar[:K-1])
+            sum2=np.sum(np.power(tn-t_pos[:i+1],H_tn[i])*randVar[K:K+i+1])
+            B[i+1]=(1/(sps.gamma(H_tn[i]+.5))*(1/np.sqrt(L))*(sum1+sum2)/np.sqrt(var[letter-1,i+1]))
+        return B
+    
+    def plotEncryptedLetter(self,samplePath):
+        plt.plot(samplePath)
         plt.show()
-     
-nbrOfSam=30
-Xsam=genSamples(nbrOfSam)
-sim_var=getVar(nbrOfSam,Xsam)
+        
+    def plotEncryptedWord(self,word):
+        for i in range(np.size(word,0)):
+            plt.plot(word[i,:])
+            plt.show()
+        
+class SampleSet(object):
+    
+    def __init__(self):
+        self.M=26
+        self.L=500
+        self.K=10*self.L
+        self.t_pos=np.linspace(0,1,self.L)
+        self.t_neg=np.linspace(-10,0,10*self.L)
+        self.t=np.union1d(self.t_neg,self.t_pos)
+    
+    def genSamplesWithOutNorm(self,n):
+        M=self.M
+        L=self.L
 
-Xsam_Uvar=genSamplesVar(nbrOfSam,sim_var)
-#sim_varU=getVar(nbrOfSam,Xsam_Uvar)
-#samplePlot(1,sim_varU)
-filename='DATA_50'
-save(Xsam,filename)
-end = time.time()
-print(end - start)
+        samples=np.zeros([n*M,L])
+        s=Encrypter()
+        for m in range(M):
+            for j in range(n):
+                samples[m*n+j,:]=s.encryptWithOutNorm(m+1)
+        return samples
+
+    def genSamples(self,n,var):
+        M=self.M
+        L=self.L
+        
+        samples=np.zeros([n*M,L])
+        s=Encrypter()
+        for m in range(M):
+            for j in range(n):
+                samples[m*n+j,:]=s.encrypter(m+1,var)
+        return samples
+    
+    def save(self,saveData,filename):
+        ofile = open(filename, "w")
+        json.dump(saveData.tolist(), ofile, sort_keys = True, indent = 4)
+        ofile.close()
+        
+    def samplePlot(self,nbrOfsam,samples):
+        M=self.M
+        
+        for m in range(M):
+            print('-------Sample path of one letter in a sample batch--------',m+1)
+            plt.plot(samples[m*nbrOfsam,:])
+            plt.show()
+
+class SimulateVar(object):
+    
+    def __init__(self):
+        self.M=26
+        self.L=500
+        self.K=10*self.L
+        self.t_pos=np.linspace(0,1,self.L)
+        self.t_neg=np.linspace(-10,0,10*self.L)
+        self.t=np.union1d(self.t_neg,self.t_pos)
+    
+    def getVar(self,nbrOfSam,samples):
+        M=self.M
+        L=self.L
+        simVar=np.zeros([M,L])
+        for m in range(M):
+            for l in range(0,L):
+                simVar[m,l]=np.var(samples[m*nbrOfSam:(m*nbrOfSam+nbrOfSam),l])
+        return simVar
+    
+    def normSet(self,n,sampleSet):
+        M=self.M
+        L=self.L
+        
+        norm=np.zeros([M,L])
+        for m in range(M):
+            for l in range(L):
+                norm[m,l]=np.var(sampleSet[m*n:m*n+n,l])
+        
+        norm=1/np.sqrt(norm)        
+        normedSampleSet=np.zeros([n*M,L])
+        for m in range(M):
+            normedSampleSet[m*n:m*n+n,1:]=np.multiply(sampleSet[m*n:m*n+n,1:],norm[m,1:])
+        return normedSampleSet
+
        
